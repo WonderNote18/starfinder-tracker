54f1b528de348e95b561233de187291ca287ec5e
Hector Almanza <hector.d.almanza@gmail.com>
2023-01-16 19:28:42 -0600

    Init expressJS w/ tailwindcss for basic pages

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', ((req, res) => {
+//   res.send('Hello worlds');
+// }));
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+module.exports = router;
